e2bec84aac1ebd6e1fff3bbcb8d3a7c8072e869c
ether <ether@192.168.1.150>
2017-05-30 16:42:47 +0200

    SuperClase Enemigo creada

---
+++ src/net/juanxxiii/j23gameengine/Enemigo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.juanxxiii.j23gameengine;
+
+/**
+ *
+ * @author Raquel
+ */
+public class Enemigo {
+    private int coord_x;
+    private int coord_y;
+    
+}
