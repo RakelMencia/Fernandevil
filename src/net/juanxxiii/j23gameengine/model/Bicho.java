1b44fbb58a58e0a7d7fc2a0d7c2f12fa6e74eba8
dam <dam@Aulaxx-yy>
2017-06-01 16:39:33 +0200

    16:24

--- src/net/juanxxiii/j23gameengine/model/Bicho.java
+++ src/net/juanxxiii/j23gameengine/model/Bicho.java
@@ -6,12 +6,15 @@
 package net.juanxxiii.j23gameengine.model;
 
 import java.io.IOException;
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author dam
  */
-public class Bicho extends Enemigo {
+public class Bicho extends Enemigo implements Runnable{
     
     public Bicho(int _x, int _y, String _icono, int _vida, int _velocidad) throws IOException {
         super(_x, _y, _icono, _vida, _velocidad);
@@ -20,6 +23,18 @@ public class Bicho extends Enemigo {
     @Override
     public void recibirImpacto() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            coord_x++;
+            try {
+                sleep(100);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Bicho.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
     
 }
