a21254913ba50fe1e162eab4c00e94032b9bb221
dam <dam@Aulaxx-yy>
2017-06-01 16:17:06 +0200

    Pinta malo Elen

--- src/net/juanxxiii/j23gameengine/model/Devil.java
+++ src/net/juanxxiii/j23gameengine/model/Devil.java
@@ -7,12 +7,15 @@ package net.juanxxiii.j23gameengine.model;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author dam
  */
-public class Devil extends Enemigo {
+public class Devil extends Enemigo implements Runnable {
 
     public Devil(int _x, int _y, String imagen, int _vida, int _velocidad) throws IOException {
         super(_x, _y, imagen, _vida, _velocidad);
@@ -31,5 +34,19 @@ public class Devil extends Enemigo {
     public void recibirImpacto() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }*/
+
+    @Override
+    public void run() {
+        while(true){
+            coord_x++;
+            coord_y++;
+            try {
+                sleep(200);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Devil.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+    }
     
 }
