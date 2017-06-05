398ba434997fa51845029854c5f5c90cad6fa43d
dam1a <dam1a@Aulaxx-yy>
2017-06-05 19:15:21 +0200

    PintatePollo

--- src/net/juanxxiii/j23gameengine/model/Pollo.java
+++ src/net/juanxxiii/j23gameengine/model/Pollo.java
@@ -1,5 +1,6 @@
 package net.juanxxiii.j23gameengine.model;
 
+import java.awt.Graphics2D;
 import java.io.IOException;
 import static java.lang.Thread.sleep;
 import java.util.logging.Level;
@@ -9,7 +10,7 @@ import java.util.logging.Logger;
  *
  * @author Vitalius Miksta
  */
-public class Pollo extends Enemigo implements Runnable{
+public class Pollo extends Enemigo implements Runnable,EnemigoI{
 
     public Pollo(int _x, int _y, String imagen, int _vida, int _velocidad) throws IOException {
         super(_x, _y, imagen, _vida, _velocidad);
@@ -18,10 +19,17 @@ public class Pollo extends Enemigo implements Runnable{
     @Override
     public void run() {
         while(true){
-            coord_x++;
-            coord_y++;
             try {
-                sleep(50);
+                coord_x++;
+                coord_y++;
+            
+                if (coord_y%2==0){
+                    coord_y=coord_y--;
+                    sleep(100);
+                }
+
+
+                    sleep(50);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Pollo.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -31,6 +39,11 @@ public class Pollo extends Enemigo implements Runnable{
     @Override
     public void recibirImpacto() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void dibujate(Graphics2D g2d) {
+        g2d.drawImage(imagen, coord_x, coord_y, null);
     }
     
     
